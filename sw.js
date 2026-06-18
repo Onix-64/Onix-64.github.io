c003706852df9a0b499f7ae727ddf2add6e54125
Onix-64 <onix64@hotmail.fr>
2026-06-18 15:48:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'foot-mardi-v10';
+const CACHE_NAME = 'foot-mardi-v12';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/logo-dark.png', '/logo-light.png', '/ball-dark.png', '/ball-light.png'];
 
 self.addEventListener('install', event => {
