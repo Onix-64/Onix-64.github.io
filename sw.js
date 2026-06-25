ed6405aae7261c0f994a2e9764d67d5c803e6457
Onix-64 <onix64@hotmail.fr>
2026-06-25 15:07:17 +0200

    Update CACHE_NAME to foot-mardi-v25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'foot-mardi-v24';
+const CACHE_NAME = 'foot-mardi-v25';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/logo-dark.png', '/logo-light.png', '/ball-dark.png', '/ball-light.png', '/pin-ball-dark-v2.png', '/pin-ball-light.png'];
 
 self.addEventListener('install', event => {
