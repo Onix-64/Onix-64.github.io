c226112d15aa56da892238ac475818182a156f4f
Onix-64 <onix64@hotmail.fr>
2026-06-26 14:41:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'foot-mardi-v32';
+const CACHE_NAME = 'foot-mardi-v33';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/logo-dark.png', '/logo-light.png', '/ball-dark.png', '/ball-light.png', '/pin-ball-dark-v2.png', '/pin-ball-light.png'];
 
 self.addEventListener('install', event => {
